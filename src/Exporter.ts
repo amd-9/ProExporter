56baee20da5a118c877fdccfb77738fe3a57462e
Anton Samoshin <asamoshin9@gmail.com>
2023-06-14 14:55:51 +0300

    Update exporter logic

--- src/Exporter.ts
+++ src/Exporter.ts
@@ -1,16 +1,13 @@
 import * as SDK from "azure-devops-extension-sdk";
 import { CommonServiceIds, getClient, IHostPageLayoutService, IProjectPageService, IProjectInfo } from "azure-devops-extension-api";
-import { TestPlanRestClient, TestPlan } from "azure-devops-extension-api/TestPlan";
+import { TestPlanRestClient, TestSuite, TestCase } from "azure-devops-extension-api/TestPlan";
+import { PagedList } from "azure-devops-extension-api/WebApi/WebApi";
+import { xml2json } from 'xml-js';
+
 
 SDK.register("export-test-data", () => {
     return {
         execute: async (context: any) => {
-
-            // const { project } = context
-
-            // const result = await getClient(TestPlanRestClient).getTestPlanById()
-            
-            // getDefinition(context.project.id, context.id, undefined, undefined, undefined, true);
 
             const dialogSvc = await SDK.getService<IHostPageLayoutService>(CommonServiceIds.HostPageLayoutService);
             const projectSvc = await SDK.getService<IProjectPageService>(CommonServiceIds.ProjectPageService);
@@ -22,9 +19,28 @@ SDK.register("export-test-data", () => {
                 return;
             }
 
-            const testPlan: TestPlan = await getClient(TestPlanRestClient).getTestPlanById(currentProject.id, context.plan.id);
+            const testPlanClient = getClient(TestPlanRestClient);
+            const testSuites : PagedList<TestSuite> = await testPlanClient.getTestSuitesForPlan(currentProject.id, context.plan.id);
+
+            let suitesData: any[] = [];           
+
+            for (let suite of testSuites) {
+                const suiteTestCases: PagedList<TestCase> = await testPlanClient.getTestCaseList(currentProject.id, context.plan.id, suite.id);
+
+                const casesWithSteps = suiteTestCases.reduce((acc, { workItem }) => {
+                    const { name, workItemFields } = workItem;
+                    const stepsField = workItemFields.find(field => field['Microsoft.VSTS.TCM.Steps']);
+                    const stepsData = xml2json(stepsField['Microsoft.VSTS.TCM.Steps']);
+
+                    return [ ...acc, { name, steps: stepsData}];
+                }, [] as any []);
+
+
+                suitesData = [ ...suitesData, { suiteName: suite.name, testCases: casesWithSteps}] 
+            }
+
  
-            dialogSvc.openMessageDialog(`Current context is ${JSON.stringify(context)} plan data is ${JSON.stringify(testPlan)}` , { showCancel: false });
+            dialogSvc.openMessageDialog(`${JSON.stringify(suitesData)}` , { showCancel: false });
         }
     }
 });
